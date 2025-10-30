ada7dfe01dd9b01c056687f5b1a4ec8422cba4aa
jdechko <62681569+jdechko@users.noreply.github.com>
2025-10-30 11:15:37 -0400

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,13 +1,6 @@
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
-alias reloadshell="omz reload"
-alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
-alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-alias compile="commit 'compile'"
-alias timestamp="date +%s"
-alias version="commit 'version'"
 alias py="python3"
 alias python="python3"
 
