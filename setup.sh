fd44191c1a1e972dc55cea60d86df331fd868b8e
Jonathan Dechko <jdechko@mac.com>
2025-11-07 16:01:05 -0500

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ brew update
 brew bundle --file ./Brewfile
 
 # Symlink the Mackup config file to the home directory
-ln -s ./.mackup.cfg $HOME/.mackup.cfg
+ln -s ~/.dotfiles/.mackup.cfg ~/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source ./.macos
